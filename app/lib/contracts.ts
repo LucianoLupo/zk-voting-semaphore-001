9455bc83334c96173f54e1112a5830273d408796
Luciano Lupo <lucianolupo97@gmail.com>
2025-10-08 18:57:43 -0300

    fix build errors

--- app/lib/contracts.ts
+++ app/lib/contracts.ts
@@ -3,4 +3,4 @@ import votingAbi from "./abi.json";
 export const VOTING_ADDRESS = (process.env.NEXT_PUBLIC_VOTING_ADDRESS || "") as `0x${string}`;
 export const SEMAPHORE_ADDRESS = (process.env.NEXT_PUBLIC_SEMAPHORE_ADDRESS || "") as `0x${string}`;
 
-export const VOTING_ABI = votingAbi as const;
+export const VOTING_ABI = votingAbi;

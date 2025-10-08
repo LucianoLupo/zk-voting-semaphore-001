2ef2a5dbe7f8c851457e910cc94791b204f013e6
Lautaro Di Sanza <lautarod@gmail.com>
2025-10-08 18:00:24 -0300

    Add - Chain Eth Sepolia

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -1,13 +1,13 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { baseSepolia, mainnet } from "wagmi/chains";
+import { baseSepolia, mainnet,sepolia } from "wagmi/chains";
 
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "YOUR_PROJECT_ID";
 
 export const config = getDefaultConfig({
   appName: "ZK Voting dApp",
   projectId,
-  chains: [baseSepolia, mainnet],
+  chains: [baseSepolia, mainnet,sepolia],
   ssr: true,
 });
